5c1add396b6209e183e2400d64a7b54ad74ac32e
Miftah Afina <miftah.afina@gmail.com>
2021-11-20 11:39:48 +0700

    Add doa

---
+++ doa_list.py
@@ -0,0 +1,219 @@
+doa_list = [
+    {
+        "id"            : "",
+        "nama"          : "Doa Sebelum Tidur",
+        "lafal"         : "بِاسْمِكَ اللَّهُمَّ أَمُوْتُ وَأَحْيَا",
+        "transliterasi" : "Bismika-llaahumma amuutu wa ahyaa.",
+        "arti"          : "Dengan Nama-Mu ya Allah, aku mati dan aku hidup.",
+        "riwayat"       : "HR. Bukhari 6324.",
+        "keterangan"    : None,
+        "kata_kunci"    : []
+    },
+    {
+        "id"            : "",
+        "nama"          : "Doa Bangun Tidur",
+        "lafal"         : "اَلْحَمْدُ لِلَّهِ الَّذِيْ أَحْيَانَا بَعْدَ مَا أَمَاتَنَا وَإِلَيْهِ النُّشُوْرِ",
+        "transliterasi" : "Alhamdulillaahil-ladzii ahyaanaa ba'da maa amaatanaa wa ilaihin-nusyuur.",
+        "arti"          : "Segala puji bagi Allah, yang membangunkan kami setelah ditidurkanNya dan kepadaNya kami dibangitkan.",
+        "riwayat"       : "HR. Al-Bukhari dalam Fathul Baari 11/113, Muslim 4/2083.",
+        "keterangan"    : None,
+        "kata_kunci"    : []
+    },
+    {
+        "id"            : "",
+        "nama"          : "Doa Ketika Mengenakan Pakaian",
+        "lafal"         : "اَلْحَمْدُ لِلَّهِ الَّذِيْ كَسَانِيْ هَـٰذَا (الثَّوْبَ) وَرَزَقَنِيْهِ مِنْ غَيْرِ حَوْلٍ مِنِّيْ وَلاَ قُوَّةٍ",
+        "transliterasi" : "Alhamdulillaahil-ladzii kasaanii haadzats-tsauba wa rozaqoniihi min ghoiri haulin minnii wa laa quwwah.",
+        "arti"          : "Segala puji bagi Allah yang memberi pakaian ini kepadaku sebagai rezeki dariNya tanpa daya dan kekuatan dariku.",
+        "riwayat"       : "HR. Seluruh penyusun kitab Sunan, kecuali An-Nasai, lihat kitab Irwa'ul Ghalil 7/47.",
+        "keterangan"    : None,
+        "kata_kunci"    : []
+    },
+    {
+        "id"            : "",
+        "nama"          : "Doa Ketika Mengenakan Pakaian Baru",
+        "lafal"         : "اَللَّهُمَّ لَكَ الْحَمْدُ أَنْتَ كَسَوْتَنِيْهِ، أَسْأَلُكَ مِنْ خَيْرِهِ وَخَيْرِ مَا صُنِعَ لَهُ، وَأَعُوْذُ بِكَ مِنْ شَرِّهِ وَشَرِّ مَا صُنِعَ لَهُ",
+        "transliterasi" : "Allaahumma lakal hamdu anta kasautaniihi, as-aluka min khoirihi wa khoiri maa shuni'a lahu, wa a'uudzu bika min syarrihi wa syarri maa shuni'a lah.",
+        "arti"          : "Ya Allah, hanya milikMu segala puji, Engkaulah yang memberi pakaian ini kepadaku. Aku mohon kepadaMu untuk memperoleh kebaikannya dan kebaikan yang ia diciptakan karenanya. Aku berlindung kepadaMu dari kejahatannya dan kejahatan yang ia diciptakan karenanya.",
+        "riwayat"       : "HR. Abu Dawud, At-Tirmidzi, Al-Baghawi dan lihat Mukhtashar Syamaailit Tirmidzi, oleh Al-Albani, halaman 47.",
+        "keterangan"    : None,
+        "kata_kunci"    : []
+    },
+    {
+        "id"            : "",
+        "nama"          : "Doa Kepada Orang yang Mengenakan Pakaian Baru 1",
+        "lafal"         : "تُبْلِي وَيُخْلِفُ اللَّهُ تَعَالَى",
+        "transliterasi" : "Tublii wa yukhlifullaahu ta'aalaa",
+        "arti"          : "Kenakanlah sampai lusuh, semoga Allah ta'ala memberikan gantinya kepadamu.",
+        "riwayat"       : "HR. Abu Daud 4/41 dan lihat pula Shahih Abi Dawud, 2/760.",
+        "keterangan"    : None,
+        "kata_kunci"    : []
+    },
+    {
+        "id"            : "",
+        "nama"          : "Doa Kepada Orang yang Mengenakan Pakaian Baru 2",
+        "lafal"         : "اِلْبَسْ جَدِيْدًا، وَعِشْ حَمِيْدًا، وَمُتْ شَهِيْدًا",
+        "transliterasi" : "Ilbas jadiidan, wa 'isy hamiidan, wa mut syahiidan.",
+        "arti"          : "Berpakaianlah yang baru, hiduplah dengan terpuji dan matilah dalam keadaan syahid.",
+        "riwayat"       : "HR. Ibnu Majah 2/1178, Al-Baghawi 12/41 dan lihat Shahih Ibnu Majah 2/275.",
+        "keterangan"    : None,
+        "kata_kunci"    : []
+    },
+    {
+        "id"            : "",
+        "nama"          : "Doa Ketika Melepas atau Meletakkan Pakaian",
+        "lafal"         : "بِسْمِ اللَّهِ",
+        "transliterasi" : "Bismillaah.",
+        "arti"          : "Dengan nama Allah (aku meletakkan baju).",
+        "riwayat"       : "HR. At-Tirmidzi 2/505 dan Imam yang lain. Lihat Irwa'ul Ghalil, 49 dan Shahihul Jami' 3/203.",
+        "keterangan"    : None,
+        "kata_kunci"    : []
+    },
+    {
+        "id"            : "",
+        "nama"          : "Doa Masuk WC atau Kamar Mandi",
+        "lafal"         : "(بِسْمِ اللَّهِ) اَللَّهُمَّ إِنِّيْ أَعُوْذُ بِكَ مِنَ الْخُبُثِ وَالْخَبَائِثِ",
+        "transliterasi" : "(Bismillaah) Allaahumma innii a'uudzu bika minal khubutsi wal khobaa-its.",
+        "arti"          : "Dengan nama Allah. Ya Allah, sesungguhnya aku berlindung kepadaMu dari godaan setan laki-laki dan perempuan.",
+        "riwayat"       : "HR. Al-Bukhari 1/45 dan Muslim 1/283. Sedang tambahan bismillaah pada permulaan hadits, menurut riwayat Said bin Manshur. Lihat Fathul Baari 1/244.",
+        "keterangan"    : None,
+        "kata_kunci"    : []
+    },
+    {
+        "id"            : "",
+        "nama"          : "Doa Keluar dari WC atau Kamar Mandi",
+        "lafal"         : "غُفْرَانَكَ",
+        "transliterasi" : "Ghufroonak.",
+        "arti"          : "Aku minta ampun kepadaMu.",
+        "riwayat"       : "HR. Seluruh penyusun kitab Sunan, kecuali An-Nasai yang meriwayatkan dalam 'Amalul Yaumi wal Lailah, lihat Takhrij Zaadul Ma'aad 2/387.",
+        "keterangan"    : None,
+        "kata_kunci"    : []
+    },
+    {
+        "id"            : "",
+        "nama"          : "Doa Sebelum Wudhu",
+        "lafal"         : "بِسْمِ اللَّهِ",
+        "transliterasi" : "Bismillaah.",
+        "arti"          : "Dengan nama Allah (aku berwudhu).",
+        "riwayat"       : "HR. Abu Dawud, Ibnu Majah dan Ahmad. Lihat Irwa'ul Ghalil 1/122.",
+        "keterangan"    : None,
+        "kata_kunci"    : []
+    },
+    {
+        "id"            : "",
+        "nama"          : "Doa Setelah Wudhu 1",
+        "lafal"         : "أَشْهَدُ أَنْ لاَ إِلَـٰهَ إِلاَّ اللَّهُ، وَحْدَهُ لاَ شَرِيْكَ لَهُ، وَأَشْهَدُ أَنَّ مُحَمَّدًا عَبْدُهُ وَرَسُوْلُهُ",
+        "transliterasi" : "Asyhadu al-laa ilaaha illallaah, wahdahu laa syariika lah, wa asyhadu anna muhammadan 'abduhu wa rosuuluh.",
+        "arti"          : "Aku bersaksi, bahwa tidak ada sesembahan yang berhak disembah kecuali Allah, Yang Maha Esa dan tiada sekutu bagiNya. Aku bersaksi, bahwa Muhammad adalah hamba dan utusanNya.",
+        "riwayat"       : "HR. Muslim 1/209.",
+        "keterangan"    : None,
+        "kata_kunci"    : []
+    },
+    {
+        "id"            : "",
+        "nama"          : "Doa Setelah Wudhu 2",
+        "lafal"         : "اَللَّهُمَّ اجْعَلْنِيْ مِنَ التَّوَّابِيْنَ وَاجْعَلْنِيْ مِنَ الْمُتَطَهِّرِيْنَ",
+        "transliterasi" : "Allaahummaj 'alnii minat-tawwaabiina waj 'alnii minal mutathohhiriin.",
+        "arti"          : "Ya Allah, jadikanlah aku termasuk orang-orang yang bertaubat dan jadikanlah aku termasuk orang-orang (yang senang) bersuci.",
+        "riwayat"       : "HR. At-Tirmidzi 1/78, dan lihat Shahih At-Tirmidzi 1/18.",
+        "keterangan"    : None,
+        "kata_kunci"    : []
+    },
+    {
+        "id"            : "",
+        "nama"          : "Doa Setelah Wudhu 3",
+        "lafal"         : "سُبْحَانَكَ اللَّهُمَّ وَبِحَمْدِكَ، أَشْهَدُ أَنْ لاَ إِلَـٰهَ إِلاَّ أَنْتَ، أَسْتَغْفِرُكَ، وَأَتُوْبُ إِلَيْكَ",
+        "transliterasi" : "Subhaanakallaahumma wa bihamdika, asyhadu al-laa ilaaha illaa anta, astaghfiruka, wa atuubu ilaik.",
+        "arti"          : "Maha Suci Engkau ya Allah, aku memuji-Mu. Aku bersaksi bahwa tidak ada sesembahan yang berhak disembah kecuali Engkau, aku minta ampun dan bertaubat kepada-Mu.",
+        "riwayat"       : "HR. An-Nasai dalam 'Amalul Yaumi wal Lailah, halaman 173 dan lihat Irwa'ul Ghalil, 1/135 dan 2/94.",
+        "keterangan"    : None,
+        "kata_kunci"    : []
+    },
+    {
+        "id"            : "",
+        "nama"          : "Doa Ketika Keluar Rumah 1",
+        "lafal"         : "بِسْمِ اللَّهِ، تَوَكَّلْتُ عَلَى اللَّهِ، لاَ حَوْلَ وَلاَ قُوَّةَ إِلاَّ بِاللَّهِ",
+        "transliterasi" : "Bismillaah, tawakkaltu 'alallaah, laa haula wa laa quwwata illaa billaah.",
+        "arti"          : "Dengan nama Allah, aku bertawakkal kepada Allah, tiada daya dan kekuatan kecuali dengan pertolongan Allah.",
+        "riwayat"       : "HR. Abu Daud 5095, Turmudzi 3426 dan dishahihkan al-Albani.",
+        "keterangan"    : None,
+        "kata_kunci"    : []
+    },
+    {
+        "id"            : "",
+        "nama"          : "Doa Ketika Keluar Rumah 2",
+        "lafal"         : "اَللَّهُمَّ إِنِّيْ أَعُوْذُ بِكَ أَنْ أَضِلَّ أَوْ أُضَلَّ، أَوْ أَزِلَّ أَوْ أُزَلَّ، أَوْ أَظْلِمَ أَوْ أُظْلَمَ، أَوْ أَجْهَلَ أَوْ يُجْهَلَ عَلَيَّ",
+        "transliterasi" : "Allaahumma innii a'uudzu bika an adhilla au udholla, au azilla au uzalla, au azhlima au uzhlama, au ajhala au yujhala 'alayya.",
+        "arti"          : "Ya Allah, aku berlindung kepada-Mu, jangan sampai aku sesat atau disesatkan (setan atau orang yang berwatak setan), berbuat kesalahan atau disalahi, menganiaya atau dianiaya (orang), dan berbuat bodoh atau dibodohi.",
+        "riwayat"       : "HR. Abu Dawud 5094, Nasai 5486, Ibnu Majah 3884 dan dishahihkan al-Albani.",
+        "keterangan"    : None,
+        "kata_kunci"    : []
+    },
+    {
+        "id"            : "",
+        "nama"          : "Doa Masuk Rumah Ketika Kosong atau Sepi",
+        "lafal"         : "اَلسَّلَامُ عَلَيْنَا وَعَلَى عِبَادِ اللَّهِ الصَّالِحِيْنَ",
+        "transliterasi" : "As-salaamu 'alainaa wa 'alaa 'ibaadillaahish-shoolihiin.",
+        "arti"          : "Semoga keselamatan atas kami dan hamba-hamba Allah yang shalih.",
+        "riwayat"       : "HR. Bukhari dalam Adabul Mufrod 806/1055. Sanad hadits ini hasan sebagaimana dikatakan oleh Al Hafizh Ibnu Hajar dalam Al Fath, 11: 17.",
+        "keterangan"    : None,
+        "kata_kunci"    : []
+    },
+    {
+        "id"            : "",
+        "nama"          : "Doa Ketika Pergi Ke Masjid",
+        "lafal"         : "اَللَّهُمَّ اجْعَلْ فِيْ قَلْبِيْ نُوْرًا، وَفِيْ لِسَانِيْ نُوْرًا، وَاجْعَلْ فِيْ سَمْعِيْ نُوْرًا، وَاجْعَلْ فِيْ بَصَرِيْ نُوْرًا، وَاجْعَلْ مِنْ خَلْفِيْ نُوْرًا، وَمِنْ أَمَامِيْ نُوْرًا، وَاجْعَلْ مِنْ فَوْقِيْ نُوْرًا، وَمِنْ تَحْتِيْ نُوْرًا، اَللَّهُمَّ أَعْطِنِيْ نُوْرًا",
+        "transliterasi" : "Allaahummaj'al fii qolbii nuuron, wa fii lisaanii nuuron, waj'al fii sam'ii nuuron, waj'al fii bashorii nuuron, waj'al min kholfii nuuron, wa min amaamii nuuron, waj'al min fauqii nuuron, wa min tahtii nuuron, allaahumma a'thinii nuuron.",
+        "arti"          : "Ya Allah, jadikanlahlah cahaya di hatiku, cahaya di lidahku, cahaya di pendengaranku, cahaya di penglihatanku, cahaya dari belakangku, cahaya dari hadapanku, cahaya dari atasku, cahaya dari bawahku. Ya Allah, berilah aku cahaya.",
+        "riwayat"       : "HR. Muslim no. 763 (191) Syarah Muslim V/51 dan lafal hadits menurut riwayatnya. Diriwayatkan juga oleh Al-Bukhari/Fat-hul Baari XI/116, banyak tambahan di dalamnya.",
+        "keterangan"    : None,
+        "kata_kunci"    : []
+    },
+    {
+        "id"            : "",
+        "nama"          : "Doa Masuk Masjid",
+        "lafal"         : "أَعُوْذُ بِاللَّهِ الْعَظِيْمِ، وَبِوَجْهِهِ الْكَرِيْمِ، وَسُلْطَانِهِ الْقَدِيْمِ، مِنَ الشَّيْطَانِ الرَّجِيْمِ، (بِسْمِ اللَّهِ، وَالصَّلاَةُ) (وَالسَّلاَمُ عَلَى رَسُوْلِ اللَّهِ) اَللَّهُمَّ افْتَحْ لِيْ أَبْوَابَ رَحْمَتِكَ",
+        "transliterasi" : "A'uudzu billaahil 'azhiim, wa biwajhihil kariim, wa sulthoonihil qodiim, minasy-syaithoonir-rojiim, (bismillaah, wash-sholaaatu) (was-salaamu 'alaa rosuulillaah) allaahummaftah lii abwaaba rohmatik.",
+        "arti"          : "Aku berlindung kepada Allah Yang Maha Agung, dengan wajahNya Yang Mulia dan kekuasaanNya yang abadi, dari setan yang terkutuk [1] Dengan nama Allah dan semoga shalawat [2] dan salam tercurahkan kepada Rasulullah [3] Ya Allah, bukalah pintu-pintu rahmatMu untukku [4].",
+        "riwayat"       : "HR. Muslim 1/494.",
+        "keterangan"    : None,
+        "kata_kunci"    : []
+    },
+    {
+        "id"            : "",
+        "nama"          : "Doa Keluar Masjid",
+        "lafal"         : "بِسْمِ اللَّهِ وَالصَّلاَةُ وَالسَّلاَمُ عَلَى رَسُوْلِ اللَّهِ، اَللَّهُمَّ إِنِّيْ أَسْأَلُكَ مِنْ فَضْلِكَ، اَللَّهُمَّ اعْصِمْنِيْ مِنَ الشَّيْطَانِ الرَّجِيْمِ",
+        "transliterasi" : "Bismillaahi wash-sholaatu was-salaamu 'alaa rosuulillaah, allaahumma innii as-aluka min fadhlika, allaahumma'shimnii minasy-syaithoonir-rojiim.",
+        "arti"          : "Dengan nama Allah, semoga shalawat dan salam terlimpahkan kepada Rasulullah. Ya Allah, sesungguhnya aku minta kepadaMu dari karuniaMu. Ya Allah, peliharalah aku dari godaan setan yang terkutuk.",
+        "riwayat"       : "Tambahan: Allaahumma'shimni minasy syai-thaanir rajim, adalah riwayat Ibnu Majah. Lihat Shahih Ibnu Majah 129.",
+        "keterangan"    : None,
+        "kata_kunci"    : []
+    },
+    {
+        "id"            : "",
+        "nama"          : "Doa Ketika Mendengarkan Adzan",
+        "lafal"         : "",
+        "transliterasi" : "",
+        "arti"          : "",
+        "riwayat"       : "HR. Al-Bukhari 1/152 dan Muslim 1/288.",
+        "keterangan"    : [
+            "Seseorang yang mendengarkan adzan, hendaklah mengucapkan sebagaimana yang diucapkan oleh muadzin, kecuali dalam kalimat: Hayya 'alash shalaah dan Hayya 'alal falaah. Maka mengucapkan:",
+            "لاَ حَوْلَ وَلاَ قُوَّةَ إِلاَّ بِاللَّهِ",
+            "Laa haula wa laa quwwata illaa billaah.",
+            "Tidak ada daya dan kekuatan kecuali dengan pertolongan Allah."
+        ],
+        "kata_kunci"    : []
+    },
+    {
+        "id"            : "",
+        "nama"          : "Doa Setelah Adzan",
+        "lafal"         : "اَللَّهُمَّ رَبَّ هَـٰذِهِ الدَّعْوَةِ التَّامَّةِ، وَالصَّلاَةِ الْقَائِمَةِ، آتِ مُحَمَّدًا الْوَسِيْلَةَ وَالْفَضِيْلَةَ، وَابْعَثْهُ مَقَامًا مَحْمُوْدًا الَّذِيْ وَعَدْتَهُ",
+        "transliterasi" : "Allaahumma robba haadzihid-da'watit-taammah, wash-sholaatil qoo-imah, aati muhammadanil wasiilata wal fadhiilah, wab'ats-hu maqooman mahmuudanil-ladzii wa 'adtah.",
+        "arti"          : "Ya Allah, Tuhan Pemilik panggilan yang sempurna (adzan) ini dan shalat (wajib) yang didirikan. Berilah Al-Wasilah (derajat di Surga, yang tidak akan diberikan selain kepada Nabi) dan fadhilah kepada Muhammad. Dan bangkitkan beliau sehingga bisa menempati maqam terpuji yang telah Engkau janjikan.",
+        "riwayat"       : "HR. Al-Bukhari 1/152.",
+        "keterangan"    : [
+            "Cara membacanya adalah “…muhammadanil wasiilata…” dan “…mahmuudanil-ladzii…”, yaitu ada tambahan nun wiqoyah (dibaca “ni”)."
+        ],
+        "kata_kunci"    : []
+    },
+]
