237b70db420cb5071770a6e2793f1510b02f78d3
Miftah Afina <miftah.afina@gmail.com>
2021-11-21 08:32:27 +0700

    Generate kata kunci

--- app.py
+++ app.py
@@ -9,21 +9,39 @@ app = Flask(__name__)
 def index():
     return jsonify(doa_list)
 
+
 @app.route("/show/<id_doa>")
 def show(id_doa):
     id_doa = escape(id_doa)
     result = []
 
     for doa in doa_list:
-        if doa['id_doa'] == id_doa:
+        if doa["id_doa"] == id_doa:
             result.append(doa)
 
     return jsonify(result)
 
+
 @app.route("/stem/<query>")
 def stem(query):
-    query = escape(query)
-    factory = StemmerFactory()
-    stemmer = factory.create_stemmer()
+    query   = escape(query)
+    stemmer = StemmerFactory().create_stemmer()
 
     return stemmer.stem(query)
+
+
+@app.route("/generate-kata-kunci")
+def generate_kata_kunci():
+    kata_kunci_list = []
+    stemmer         = StemmerFactory().create_stemmer()
+
+    for doa in doa_list:
+        nama_doa_stem     = stemmer.stem(doa["nama"])
+        kata_kunci_list   = nama_doa_stem.split(" ")
+        doa["kata_kunci"] = kata_kunci_list
+
+        file = open("generated_doa_list.py", "w")
+        file.write(str(doa_list))
+        file.close
+
+    return jsonify(doa_list)
